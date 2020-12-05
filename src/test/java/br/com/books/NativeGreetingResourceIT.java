a3bd9f93c06ea50756c0d74321a4f05f1b46eeaa
Ronaldo Feitosa <rfeitosa@ciandt.com>
2020-12-05 18:02:35 -0300

    Adjust unit tests

--- src/test/java/br/com/books/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.books;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
